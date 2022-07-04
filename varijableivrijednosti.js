538d4f8a18baeba56335e5a152ba2dacfe1ed947
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-07-04 11:15:22 +0200

    Add files via upload

---
+++ varijableivrijednosti.js
@@ -0,0 +1,36 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("domagoj");
+
+let firstName = "Domagoj";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+*/
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "domagoj");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+*/
+
+let country = "hrvatska";
+let continent = "europa";
+let population = 1000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
