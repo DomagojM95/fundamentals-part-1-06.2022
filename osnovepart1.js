c463ba27b35833314047c2814ac1a3880465f8e4
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-07-06 12:15:59 +0200

    Add files via upload

---
+++ osnovepart1.js
@@ -0,0 +1,92 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("domagoj");
+
+let firstName = "Domagoj";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+*/
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "domagoj");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+
+let country = "hrvatska";
+let continent = "europa";
+let population = 1000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let age = 30;
+age = 31;
+const birthYear = 1991;
+
+console.log(birthYear);
+console.log(age);
+
+// matematicki operateri
+const now = 2037;
+const ageDomagoj = now - 1991;
+const ageEva = now - 2018;
+console.log(ageDomagoj, ageEva);
+
+console.log(ageDomagoj * 2, ageDomagoj / 10, 2 ** 3);
+
+const firstName = "Domagoj";
+const lastName = "Marković";
+console.log(firstName + " " + lastName);
+
+// assingment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x= x * 4 = 100
+x++; // x= x+1
+x--;
+x--;
+console.log(x);
+
+// comparison operateri
+
+console.log(ageDomagoj > ageEva); // > , < , >= , <=
+
+console.log(ageEva >= 18);
+
+
+
+let hrvatska = 4000000;
+
+console.log(hrvatska / 2);
+
+hrvatska++;
+console.log(hrvatska);
+
+let finland = 6000000;
+console.log(finland > hrvatska);
+
+let averagePopulation = 33000000;
+console.log(hrvatska < averagePopulation);
+
+const description =
+  "Portugal is in Europe, and its 11 million people speaks portuguese";
+
+console.log(description);
+*/
